810e9519599ba746c5e62ae57b1fb87c1b162d56
Lefebvre Steven <133742833+HamelenYakkington@users.noreply.github.com>
2025-01-31 14:15:30 +0100

    upgrade style

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,4 +1,4 @@
-import './bootstrap.js';
+// import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -6,3 +6,11 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import '../styles/app.css';
+
+// const alerts = document.querySelectorAll('[class*="alert-"]')
+// for (const alert of alerts) {
+//     setTimeout( function() {
+//         const bootstrapAlert = bootstrap.Alert.getOrCreateInstance(alert);
+//         bootstrapAlert.close();
+//     }, 2500);
+// }
